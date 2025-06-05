16257b5d0db17599e00efd0309dacf99136ee030
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2025-06-05 12:17:57 +0530

    Added Pyflyt test code

---
+++ Pyflyt/test.py
@@ -0,0 +1,12 @@
+import gymnasium
+import PyFlyt.gym_envs  # noqa
+
+env = gymnasium.make("PyFlyt/QuadX-Hover-v4", render_mode="human")
+obs = env.reset()
+
+termination = False
+truncation = False
+
+while not termination or truncation:
+    observation, reward, termination, truncation, info = env.step(
+        env.action_space.sample())
